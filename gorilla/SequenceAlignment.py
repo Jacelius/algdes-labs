7ea21a78913cfdb706274908928418949d9ad52c
Silas Paaske <sipn@itu.dk>
2022-10-05 09:54:27 +0200

    status 9:54 Co-authored-by: Benjamin Salo Kjær <kjaerb@users.noreply.github.com>

--- gorilla/SequenceAlignment.py
+++ gorilla/SequenceAlignment.py
@@ -1,3 +1,17 @@
+# Helper function to print a matrix nicely
+def show(intro, mat):
+    print("\n" + intro)
+    
+    if isinstance(mat[0], int):
+        print(mat)
+        return 0
+
+    n = len(mat)
+    m = len(mat[0])
+    for i in range(n):
+        for j in range(m):
+            print(mat[i][j], end = " " if j < m - 1 else "\n")
+
 # read txt file data/BLOSUM62.txt, to add scores to a dictionary
 scores = {}
 with open('data/BLOSUM62.txt') as f:
@@ -32,22 +46,19 @@ for line in lines:
 
 # time for the sequence alignment algorithm
 """
-(6.16)   The minimum alignment costs satisfy the following recurrence for i ≥ 1 and j ≥ 1:
+(6.16) The minimum alignment costs satisfy the following recurrence for i ≥ 1 and j ≥ 1:
 opt(i, j) = min[ixiy + opt(i - 1, j - 1), 8 + opt(i - 1,j), 8 + opt(i, j - 1)].
 Moreover, (i,j) is in an optimal alignment M for this subproblem if and only if the minimum is achieved by the first of these values.
- 
 We have maneuvered ourselves into a position where the dynamic programming algorithm has become clear: We build up the values of opt(i, j) using the recurrence in (6.16). There are only O(mn) subproblems, and OPT(m, n) is the value we are seeking.
 We now specify the algorithm to compute the value of the optimal alignment. For purposes of initialization, we note that opt(i, 0) = opt(0, i) = iδ for all i, since the only way to line up an i-letter word with a 0-letter word is to use i gaps.
 """
 
-# define 2d grid: scoring matrix
+print("dna dict", dna)
 
-print(dna)
+gap_penalty = scores['*']['A'] # -4
+double_gap_penalty = scores['*']['*'] # 1
 
-gap_penalty = scores['*']['A']
-double_gap_penalty = scores['*']['*']
-
-
+# helper function to print matrixes nicely
 def show(intro, mat):
     print("\n" + intro)
 
@@ -61,22 +72,13 @@ def show(intro, mat):
         for j in range(m):
             print(mat[i][j], end=" " if j < m - 1 else "\n")
 
-
-def opt(x: list, y: list):  # (6.16) Needleman Wunsch algorithm
-
-    max_length = max(len(x), len(y))
-
-
-def alignment(x, y):
-    max_size = max(len(x), len(y))
-    # dimensions: len(x) * len(y)
-    A = [[gap_penalty]*max_size for i in range(max_size)]
+def alignment(A, x, y):
     # Initialize A[i, 0] for each i
-    for i in range(len(x)):
-        A[i][0] = i * gap_penalty  # 8 is the gap penalty (fix)
+    for i in range(1, len(x)):
+        A[i][0] = i * gap_penalty
     # Initialize A[0, j] for each j
-    for j in range(len(y)):
-        A[0][j] = j * gap_penalty  # 8 is the gap penalty (fix)
+    for j in range(1, len(y)):
+        A[0][j] = j * gap_penalty
     # Fill in the rest of the matrix
     for i in range(len(x)):  # 0 .. n
         for j in range(len(y)):  # 0 .. m
@@ -85,7 +87,56 @@ def alignment(x, y):
                 gap_penalty + A[i-1][j],
                 gap_penalty + A[i][j-1]
             )
-    return A  # OPT(n, m)
+    return A[len(x)-1][len(y)-1]  # OPT(n, m)
+
+def fill_traceback_matrix(A):
+    # Initialize the traceback matrix
+    T = [[0 for j in range(len(y))] for i in range(len(x))]
+
+    # Define final cell in matrix
+    T[0][0] = "END"
+
+    # Initialize the first row and column
+    for i in range(1, len(x)):  # 1 .. n
+        T[i][0] = "UP"
+    for j in range(1, len(y)):  # 1 .. m
+        T[0][j] = "LEFT"
+    
+    # Fill in the rest of the matrix
+    for i in range(1, len(x)):  # 0 .. n
+        for j in range(1, len(y)):  # 0 .. m
+            if A[i][j] == scores[x[i]][y[j]] + A[i-1][j-1]:
+                T[i][j] = "DIAG"
+            elif A[i][j] == gap_penalty + A[i-1][j]:
+                T[i][j] = "UP"
+            elif A[i][j] == gap_penalty + A[i][j-1]:
+                T[i][j] = "LEFT"
+    
+    return T
+
+def traverse_traceback_matrix(T, x, y): # x and y are the sequences
+    # Initialize the aligned strings
+    x_aligned = ""
+    y_aligned = ""
+    # Start from the bottom right cell in the traceback matrix
+    i = len(x) - 1
+    j = len(y) - 1
+    # Keep going until you reach the top left cell
+    while T[i][j] != "END":
+        if T[i][j] == "DIAG":
+            x_aligned = x[i] + x_aligned
+            y_aligned = y[j] + y_aligned
+            i -= 1
+            j -= 1
+        elif T[i][j] == "UP":
+            x_aligned = x[i] + x_aligned
+            y_aligned = "-" + y_aligned
+            i -= 1
+        elif T[i][j] == "LEFT":
+            x_aligned = "-" + x_aligned
+            y_aligned = y[j] + y_aligned
+            j -= 1
+    return x_aligned, y_aligned
 
 
 for key in dna:
@@ -93,6 +144,20 @@ for key in dna:
     reduced_dna = dna.copy()
     reduced_dna.pop(key)
     for other in reduced_dna:
-        #x = list(dna[key])
-        #y = list(dna[other])
-        print(alignment(dna[key], dna[other]))
+        x = dna[key]
+        y = reduced_dna[other]
+        max_size = max(len(x), len(y))
+        A = [[gap_penalty]*max_size for i in range(max_size)] # initialize A
+        mn = alignment(A, x, y)
+        show("A for " + key +  " with " + other, A)
+        print("A[m][n]", mn)
+        # Work backwards from (N - 1, M - 1) to (0, 0)
+        # to find the best alignment.
+        T = fill_traceback_matrix(A)
+        show("T for " + key +  " with " + other, T)
+        x_aligned, y_aligned = traverse_traceback_matrix(T, x, y)
+        print("x_aligned", x_aligned)
+        print("y_aligned", y_aligned)
+        
+
+
