1b10ed2295e8e4ef796a85c11a51d06e77713c86
Silas Paaske <sipn@itu.dk>
2022-10-23 18:43:03 +0200

    flow gets stuck at 118 :|

--- flow/Flow.py
+++ flow/Flow.py
@@ -5,7 +5,7 @@ class Edge:
         self.c = c
 
     def __str__(self):
-        return str(self.u) + " -> " + str(self.v) + " : " + str(self.c)
+        return str(self.u) + "->" + str(self.v) + " c = " + str(self.c)
 
     def __repr__(self):
         return str(self)
@@ -14,33 +14,41 @@ class Graph:
     def __init__(self, num_nodes):
         self.node_map_list = []
         for i in range(num_nodes):
-            self.node_map_list.append({}) # create a dictionary for each node
+            self.node_map_list.append({}) # create a dictionary {node: Edges[]} for each node
 
 class FordFulkerson:
     def augment(self, graph, current, sink, flow, visited, threshold):
         if current == sink:
+            print("augmenting flow by " + str(flow))
             return flow
         elif (current not in visited):
             visited.add(current)
-            for next in graph.node_map_list[current]:
-                if next.c >= threshold:
-                    augmented_flow = self.augment(graph, next, sink, min(flow, next.c), visited, threshold)
-                    if augmented_flow > 0:
-                        next.c -= augmented_flow
-                        next.u.c += augmented_flow
-                        return augmented_flow
+            edges = graph.node_map_list[current].values()
+            for edge in edges:
+                if edge.c >= threshold:
+                    residual = self.augment(graph, edge.v, sink, min(flow, edge.c), visited, threshold)
+                    if residual > 0:
+                        edge.c -= residual
+                        graph.node_map_list[edge.v][edge.u].c += residual
+                        return residual
         return 0
 
     def max_flow(self, graph, source, sink):
         self.max_flow = 0
         threshold = 1
         flow = 0
+        inc = 0
+
         while threshold > 0:
+            inc = -1
+            while inc != 0:
+                inc = self.augment(graph, source, sink, float('inf'), set(), threshold)
+                flow += inc
+                """                 if flow == 118:
+                    print("118 now")
+                    print("flow = " + str(flow))
+                # print("flow = " + str(flow)) """
             threshold /= 2
-            while True:
-                flow += self.augment(graph, source, sink, float('inf'), set(), threshold)
-                if flow == 0:
-                    break
         self.max_flow = flow
 
     def __init__(self, graph, source, sink):
@@ -64,12 +72,18 @@ num_arcs = int(lines[num_nodes+1]) # m
 g = Graph(num_arcs*2)
 
 for i in range(num_nodes+2, num_nodes+2+num_arcs):
-    u, v, c = lines[i].split() # [u, v, c] 
-    g.node_map_list[int(u)][int(v)] = Edge(u, v, c) # add edge to graph
-    g.node_map_list[int(v)][int(u)] = Edge(u, v, c) # add reverse edge to graph
+    u, v, c = lines[i].split() # [u, v, c]
+    if c == "-1":
+        c = float('inf')
+        g.node_map_list[int(u)][int(v)] = Edge(int(u), int(v), c) # add edge to graph
+        g.node_map_list[int(v)][int(u)] = Edge(int(u), int(v), c) # add reverse edge to graph
+    else:
+        g.node_map_list[int(u)][int(v)] = Edge(int(u), int(v), int(c)) # add edge to graph
+        g.node_map_list[int(v)][int(u)] = Edge(int(u), int(v), int(c)) # add reverse edge to graph
 
-print(g.node_map_list)
+# print(g.node_map_list)
 
+print("Ford-Fulkerson running on graph with", num_nodes, "nodes and", num_arcs, "arcs")
 FordFulkerson = FordFulkerson(g, 0, 54)
 print("max flow: ", FordFulkerson.max_flow)
 
