6db9f16a9f18145d360459029f49d56f23e4e176
Silas Paaske <sipn@itu.dk>
2022-11-07 09:15:07 +0100

    scared of red-scare 👻

--- red-scare/None.py
+++ red-scare/None.py
@@ -1,4 +1,6 @@
 # return the length of the shortest s, t path
+# If no such path exists, return ‘-1’.
 # has to avoid red nodes (nodes in R)
-def shortest_path(G, s, t, R):s
+def shortest_path(G, s, t, R):
+    # idea: remove red nodes from G, then run shortest path algorithm
     pass
--- red-scare/main.py
+++ red-scare/main.py
@@ -1,6 +1,24 @@
-# get a graph
+# parse graphs
+def parse_common_graph():
+    with ("red-scare/data/common-1-3000.txt") as f:
+        g = {} 
+        print("f", f)
+        lines = f.readlines()
+        num_nodes, num_edges, idk = lines[0].split()
+        start_node, end_node = lines[1].split() # "start", "ender"
+        for i in range(2, num_nodes+2):
+            node_id, node_type = lines[i].split()
+            # add node to graph
+            g[node_id] = node_type
+        print("g", g)
+        # what do the stars mean for the words?
 
+parse_common_graph()
+
+    
 # run None, Some, Many, Few & Alternate on the graph 
 # it is allowed to run on some well defined class of graph only (e.g. all bipartite, acyclic, directed, or simply all graphs)
 
 # repeat for all graph types
+
+
